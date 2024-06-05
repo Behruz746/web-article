79c552d9d646eea995d33e9bd5a999a77fc257d3
Behruz <madaminovbehruz746@gmail.com>
2024-06-05 15:29:56 +0500

    added: register functions

--- src/slice/auth.js
+++ src/slice/auth.js
@@ -11,14 +11,25 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    // Login
     loginUserStart: (state) => {
       state.isLoad = true;
     },
     loginUserSuccess: (state) => {},
     loginUserFailure: (state) => {},
+    // Register
+    registerUserStart: (state) => {
+      state.isLoad = true;
+    },
+    registerUserSuccess: (state) => {},
+    registerUserFailure: (state) => {},
   },
 });
 
-export const { loginUserStart, loginUserSuccess, loginUserFailure } =
-  authSlice.actions;
+export const {
+  loginUserStart,
+  loginUserSuccess,
+  loginUserFailure,
+  registerUserStart,
+} = authSlice.actions;
 export default authSlice.reducer;
