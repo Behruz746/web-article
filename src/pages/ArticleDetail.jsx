6e2f0f5509e17a14d6d1bbd1254e66a7db20c340
Behruz <madaminovbehruz746@gmail.com>
2024-06-11 15:14:43 +0500

    added: article detail page

--- src/pages/ArticleDetail.jsx
+++ src/pages/ArticleDetail.jsx
@@ -2,15 +2,17 @@ import React, { useEffect } from "react";
 import userData from "../service/data";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { Loader } from "../ui";
 import {
   getArticleDetailStart,
   getArticleDetailSuccess,
   getArticleDetailFailure,
 } from "../slice/articel";
+import moment from "moment";
 
 function Article() {
   const { id } = useParams();
-  const { articleDetail } = useSelector((state) => state.article);
+  const { articleDetail, isLoad } = useSelector((state) => state.article);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,14 +29,37 @@ function Article() {
     getArticle();
   }, [id]);
 
-  return (
-    <div className="container">
-      <div className="article__content">
-        <h1 className="article__title">{articleDetail?.title}</h1>
-        <p className="article__description">{articleDetail?.description}</p>
-        <p className="article__date"></p>
+  return isLoad ? (
+    <Loader />
+  ) : (
+    articleDetail !== null && (
+      <div className="container">
+        <div className="article__content">
+          <h1 className="article__title">{articleDetail.title}</h1>
+          <p className="article__description ">{articleDetail.description}</p>
+          <div>
+            {articleDetail.body}{" "}
+            <p className="article__date m-0">
+              <span className="fw-bold">created at: </span>
+              {moment(articleDetail.createdAt).format("DD MMM, YYYY")}
+            </p>
+          </div>
+          <div className="d-flex row gap-1 mt-3">
+            <p className="m-0 text-capitalize">
+              <span className="fw-bold text-lowercase">auther name:</span>{" "}
+              {articleDetail.author.username}
+            </p>
+            <p className="m-0">
+              <span className="fw-bold">auther bio:</span>{" "}
+              {articleDetail.author.bio}
+            </p>
+            <button type="button" className="btn btn-primary mt-3">
+              auther profile
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
